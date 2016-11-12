5213073c01da6cca29971d7f81c4087f0b3d957f
Shing-Yun Chiang <smilein0119@gmail.com>
2016-11-12 00:25:38 -0500

    world map

--- 10.1/script.js
+++ 10.1/script.js
@@ -12,13 +12,35 @@ var plot = d3.select('.canvas')
     .attr('transform','translate('+ m.l+','+ m.t+')');
 
 //Mapping specific functions
-//Projection
 
+//Projection
+var projection = d3.geoMercator();
+    
 //Geopath
+var pathGenerator = d3.geoPath()
+    .projection(projection); 
+
+console.log(pathGenerator);
 
 d3.json('../data/world-50m.json',dataloaded);
 
 function dataloaded(err, data){
     console.log(data); //This is a Topojson data
     console.log(topojson.feature(data,data.objects.countries)); //This is the converted GeoJSON data of countries
+
+  var countries = topojson.feature(data,data.objects.countries);
+  console.log(countries.features);
+
+  projection.fitExtent([[0,0],[w,h]],countries);
+
+  var worldMap = plot.selectAll('.countries')
+      .data(countries.features)
+      .enter()
+      .append('path').attr('class','countries')
+      .attr('d',pathGenerator)
+      .style('fill','rgb(200,200,200)')
+      .style('stroke-width','1px')
+      .style('stroke','white');
+
+
 }
